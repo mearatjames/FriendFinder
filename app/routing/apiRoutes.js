3e46acbfd9ebb7651b3560b8189aaabad48db323
Mearat Hou <mearatjames@gmail.com>
2018-10-21 14:17:47 -0700

    Set up newFriend obj and fetch POST request to the apiRoutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,10 @@ module.exports = function(app) {
         res.json(friendData)
       })
     app.post("/api/friend", function(req, res) {
+        console.log(req.body)
         
+        friendData.push(req.body)
+        res.json(friendData)
     // if (tableData.length < 5) {
     //     tableData.push(req.body);
     //     res.json(true);
