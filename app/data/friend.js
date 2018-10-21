d18ea852821a2b8671e7f086ed7e2943aa083c69
Mearat Hou <mearatjames@gmail.com>
2018-10-21 12:51:57 -0700

    Add value attribute to radio checkbox

--- app/data/friend.js
+++ app/data/friend.js
@@ -14,7 +14,104 @@ let friendArr = [
           5,
           3
       ]
-    }
+    },
+    {
+      name: "Test Friend2",
+      photo: "https://unsplash.com/photos/ccvj2ZQaYaw",
+      score: [
+          5,
+          1,
+          4,
+          5,
+          3,
+          4,
+          5,
+          1,
+          2,
+          3
+      ]
+    },
+    {
+      name: "Test Friend3",
+      photo: "https://unsplash.com/photos/ccvj2ZQaYaw",
+      score: [
+          5,
+          4,
+          4,
+          2,
+          4,
+          4,
+          5,
+          4,
+          2,
+          4
+      ]
+    },
+    {
+      name: "Test Friend4",
+      photo: "https://unsplash.com/photos/ccvj2ZQaYaw",
+      score: [
+          4,
+          2,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3,
+          3
+      ]
+    },
+    {
+      name: "Test Friend5",
+      photo: "https://unsplash.com/photos/ccvj2ZQaYaw",
+      score: [
+          1,
+          1,
+          3,
+          4,
+          2,
+          4,
+          5,
+          2,
+          3,
+          4
+      ]
+    },
+    {
+      name: "Test Friend6",
+      photo: "https://unsplash.com/photos/ccvj2ZQaYaw",
+      score: [
+          2,
+          5,
+          5,
+          3,
+          4,
+          2,
+          3,
+          5,
+          5,
+          2
+      ]
+    },
+    {
+      name: "Test Friend7",
+      photo: "https://unsplash.com/photos/ccvj2ZQaYaw",
+      score: [
+          4,
+          5,
+          3,
+          4,
+          2,
+          3,
+          4,
+          5,
+          3,
+          1
+      ]
+    },
+
   ];
   
 module.exports = friendArr
